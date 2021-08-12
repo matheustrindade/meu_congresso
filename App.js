193acbab700e7962050429a134898caa72db09b2
Matheus Santos <maatheus.trindade.santos@gmail.com>
2021-08-12 17:13:05 -0300

    adição de libs de tabs e ícones

--- App.js
+++ App.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import {useTheme} from './src/utils/theme';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icons from 'react-native-vector-icons/FontAwesome5';
 
-const App = () => {
-  useTheme();
+const Tab = createBottomTabNavigator();
 
-  return (
-    <SafeAreaView>
-      <Text style={styles.title}>meu congresso</Text>
-    </SafeAreaView>
-  );
+const TAB_ICONS = {
+  home: 'home',
+  votações: 'vote-yea',
+  perfil: 'user-circle',
+  congressistas: 'user-tie',
 };
 
 const styles = EStyleSheet.create({
-  title: {
-    color: '$colors.text.error',
+  tab: {
+    backgroundColor: 'green',
   },
 });
+
+const Home = () => <Text style={styles.title}>meu congresso</Text>;
+const App = () => {
+  useTheme();
+
+  const getTabStyles = useCallback(() => {
+    return styles.tab;
+  }, []);
+
+  const getScreenOptions = useCallback(
+    ({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        return <Icons name={TAB_ICONS[route.name]} color={color} size={size} />;
+      },
+      tabBarStyle: getTabStyles(),
+    }),
+    [getTabStyles],
+  );
+
+  return (
+    <SafeAreaView>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={getScreenOptions}>
+          <Tab.Screen name="home" component={Home} />
+          <Tab.Screen name="congressistas" component={Home} />
+          <Tab.Screen name="votações" component={Home} />
+          <Tab.Screen name="perfil" component={Home} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
 
 export default App;
