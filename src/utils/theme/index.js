5bc1ae1284bc3eb44340078eac031ae7281bc128
Matheus Santos <maatheus.trindade.santos@gmail.com>
2021-08-12 15:20:47 -0300

    hook para temas dark e light

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -1,22 +1,48 @@
-import React, {createContext, useMemo} from 'react';
+import {useCallback, useRef} from 'react';
 import spaces from './spaces.json';
 import fonts from './fonts.json';
 import colorsDark from './colors.dark.json';
 import colorsLight from './colors.light.json';
+import {useColorScheme} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-export const ThemeContext = createContext({});
+export const useTheme = () => {
+  const activeTheme = useColorScheme();
+  let prevTheme = useRef(null);
 
-export const ThemeProvider = ({children}) => {
-  const theme = useMemo(() => {
-    const colors = true ? colorsLight : colorsDark;
-    return {
-      spaces,
-      ...fonts,
-      ...colors,
-    };
+  const transformTheme = useCallback(theme => {
+    let transformedTheme = {};
+    for (let key of Object.keys(theme)) {
+      const newKey = `$${key}`;
+      transformedTheme[newKey] = theme[key];
+    }
+    return transformedTheme;
   }, []);
 
-  return (
-    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
-  );
+  const getTheme = useCallback(() => {
+    if (prevTheme.current === activeTheme) {
+      return;
+    }
+
+    const theme = {
+      spaces,
+      ...fonts,
+      colors: activeTheme === 'dark' ? colorsDark : colorsLight,
+    };
+
+    const transformedTheme = transformTheme(theme);
+
+    console.log('theme: ', transformedTheme);
+
+    prevTheme.current = activeTheme;
+
+    EStyleSheet.build(transformedTheme);
+  }, [activeTheme, transformTheme]);
+
+  return getTheme();
 };
+
+module.hot.accept(() => {
+  EStyleSheet.clearCache();
+  EStyleSheet.build();
+});
