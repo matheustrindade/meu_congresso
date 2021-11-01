12415d3957448a9719a84bf8ae1ab336d731aedb
Matheus Santos <maatheus.trindade.santos@gmail.com>
2021-11-01 16:27:53 -0300

    adicionando splash screen - precisa de uma nova imagem futuramente

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import {useTheme} from './src/utils/theme';
 import {Navigation} from './src/components/navigation/navigation.component';
+import SplashScreen from 'react-native-splash-screen';
 
 const styles = EStyleSheet.create({
   pageContainer: {
@@ -12,6 +13,10 @@ const styles = EStyleSheet.create({
 });
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   useTheme();
 
   return (
