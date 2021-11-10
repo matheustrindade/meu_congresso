1d71a07c20affa7baea4c4a348d61af2038a77e0
Matheus Santos <maatheus.trindade.santos@gmail.com>
2021-11-10 00:11:33 -0300

    finalizando estilo de input e iniciando desenvolvimento de logica de autenticaçao

--- src/components/ui/text.component.js
+++ src/components/ui/text.component.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {Text as NativeText} from 'react-native';
 import textStyle from './text.styles';
 
-export const Text = ({
-  children,
-  color = 'primary',
-  size = 'body',
-  style,
-  family = 'body',
-}) => {
-  return (
-    <NativeText style={[textStyle({size, color, family}).text, style]}>
-      {children}
-    </NativeText>
-  );
-};
+export const Text = forwardRef(
+  (
+    {children, color = 'primary', size = 'body', style, family = 'body'},
+    ref,
+  ) => {
+    return (
+      <NativeText
+        style={[textStyle({size, color, family}).text, style]}
+        ref={ref}>
+        {children}
+      </NativeText>
+    );
+  },
+);
