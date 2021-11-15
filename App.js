53f4e61136540b848f31fcc5b6cf4e69c79bb4b5
Matheus Santos <maatheus.trindade.santos@gmail.com>
2021-11-15 18:41:06 -0300

    desenvolvendo tela de login

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import {useTheme} from './src/utils/theme';
-import {Navigation as AppNavigation} from './src/components/app-navigation/app-navigation.component';
+import {AppNavigation} from './src/components/app-navigation/app-navigation.component';
 import SplashScreen from 'react-native-splash-screen';
 import {AuthenticationProvider} from './src/services/authentication.service';
 
